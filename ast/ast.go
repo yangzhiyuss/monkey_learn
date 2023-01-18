bdac2f4a7212ba576fe1b46fae230f3e0b93bbf3
杨志宇 <yangzy22091405@q1.com>
2023-01-18 15:44:46 +0800

    ast

--- ast/ast.go
+++ ast/ast.go
@@ -8,7 +8,7 @@ import (
 // Node root Node
 type Node interface {
 	TokenLiteral() string
-	String() string
+	String()       string
 }
 
 // Statement 所有的Statement(语句)继承这个语句(不产生值)
@@ -227,6 +227,60 @@ func (ie *InfixExpression) String() string {
 	out.WriteString(" " + ie.Operator + " ")
 	out.WriteString(ie.Right.String())
 	out.WriteString(")")
+
+	return out.String()
+}
+
+// IfExpression if
+type IfExpression struct {
+	Token       token.Token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode() {
+
+}
+
+func (ie *IfExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
+
+type BlockStatement struct {
+	Token     token.Token
+	Statement []Statement
+}
+
+func (bs *BlockStatement) statementNode() {
+
+}
+
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statement {
+		out.WriteString(s.String())
+	}
 
 	return out.String()
 }
